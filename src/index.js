5e6698fd6688416758be0baa939516973f27f1f4
Oz Ramos <labofoz@gmail.com>
2018-07-06 22:53:01 -0700

    #2 Moves static methods into mixins for now

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * index.js
- * - Main entrypoint for PosePointer
+ * - Main entrypoint for Posepointer
  *
  * - Usage example 1:
  *    new (require('posepointer'))(OPTIONS)
@@ -9,16 +9,15 @@
  *    <script src="./posepointer.js"></script>
  *
  * - Usage example 3:
- *    import PosePointer from 'posepointer'
- *    const posepointer = new PosePointer(OPTIONS)
+ *    import Posepointer from 'posepointer'
+ *    const posepointer = new Posepointer(OPTIONS)
  *
  *    posepointer.update(NEW_OPTIONS)
  */
-const posenet = require('@tensorflow-models/posenet')
 const util = require('./util')
 require('./polyfills')
 
-module.exports = class PosePointer {
+class Posepointer {
   /**
    * # CONSTRUCTOR
    * ## IMPORTANT METHOD: @TODO
@@ -26,7 +25,7 @@ module.exports = class PosePointer {
    * [] Sanitizes options and sets sane defaults (@IDEA maybe we can create
    *    "quickstart strings", where you can pass a string for a set of common
    *    options instead, like ['desktop', 'ios11', 'Raspberry Pi'])
-   * [] If autostart is true, then tracking is initialized if PosePointer
+   * [] If autostart is true, then tracking is initialized if Posepointer
    *    itself hasn't initialized PoseNet yet, otherwise tracking resumes
    *
    * @param {Object} [opts={}] Constructor options, @see /wiki/Options.md
@@ -50,97 +49,6 @@ module.exports = class PosePointer {
 
     // Possibly autostart
     this.options.autostart && this.start()
-  }
-
-  /**
-   * # PRIVATE METHOD
-   *
-   * Sets defaults to the missing constructor options
-   * @FIXME This could use some refactoring
-   *
-   * @param {Object} opts The options passed into the constructor
-   */
-  static setDefaults (opts) {
-    this.initOptions = opts
-
-    // Setup defaults
-    this.options = {
-      autostart: typeof opts.autostart !== 'undefined' ? opts.autostart : true,
-      facingMode: opts.facingMode || 'user',
-      video: opts.video || util.createDefaultVideo(),
-      canvas: opts.canvas || util.createDefaultCanvas(),
-      posenet: {
-        multiplier: opts.posenet && opts.posenet.multiplier ? opts.posenet.multiplier : 0.75,
-        maxUsers: opts.posenet && opts.posenet.maxUsers ? opts.posenet.maxUsers : 1,
-        minPoseConfidence: opts.posenet && opts.posenet.minPoseConfidence ? opts.posenet.minPoseConfidence : 0.1,
-        minPartConfidence: opts.posenet && opts.posenet.minPartConfidence ? opts.posenet.minPartConfidence : 0.5,
-        nmsRadius: opts.posenet && opts.posenet.nmsRadius ? opts.posenet.nmsRadius : 20
-      }
-    }
-  }
-
-  /**
-   * # PRIVATE METHOD
-   *
-   * Applies aliases to common options
-   */
-  static setAliases () {
-    this.video = this.options.video
-    this.canvas = this.options.canvas
-  }
-
-  /**
-   * # PRIVATE METHOD
-   *
-   * Sets up the webcam and stream: @TODO
-   * [] This will create its own video/canvas elements, allowing you to have
-   *    multiple instances going (for example, to use front/back cameras
-   *    simultaneously)
-   * [] Recreates the video feed to reassign srcObject
-   */
-  static async setupFeed () {
-    // Set webcam dimensions
-    this.canvas.width = this.video.width = 600
-    this.canvas.height = this.video.height = 500
-
-    // Start the stream based on the device
-    const isMobile = util.isMobile()
-    this.video.srcObject = await navigator.mediaDevices.getUserMedia({
-      // We only care about the camera
-      audio: false,
-      video: {facingMode: this.options.facingMode},
-      width: isMobile ? undefined : this.video.width,
-      height: isMobile ? undefined : this.video.height
-    })
-    this.video.play()
-  }
-
-  /**
-   * # PRIVATE METHOD
-   *
-   * Initializes PoseNet and starts the tracking loop: @TODO
-   * [] This loads a model from Google's servers based on the chosen PoseNet
-   *    modifier
-   */
-  static async initPoseNet () {
-    this.posenet = await posenet.load(this.options.posenet.multiplier)
-    this._isTracking = true
-    this.constructor.trackPosesLoop(this)
-  }
-
-  /**
-   * # PRIVATE METHOD
-   *
-   * Recursive method for tracking poses on each animationFrame: @TODO
-   * [] This method is recursive, once called it continues until after
-   *    posepointer.stop() is called or until this._isTracking is false
-   *
-   * @param {PosePointer} context The this context, since we're in the
-   *    constructor scope now
-   */
-  static async trackPosesLoop (context) {
-    context.trackPoses()
-    context._isTracking && requestAnimationFrame(() => this.trackPosesLoop(context))
   }
 
   /**
@@ -182,3 +90,6 @@ module.exports = class PosePointer {
     console.log('this.stop()')
   }
 }
+
+require('./mixins')(Posepointer)
+module.exports = Posepointer
---
+++ src/mixins.js
@@ -0,0 +1,95 @@
+const posenet = require('@tensorflow-models/posenet')
+const util = require('./util')
+
+module.exports = function (Posepointer) {
+  /**
+   * # PRIVATE METHOD
+   *
+   * Sets defaults to the missing constructor options
+   * @FIXME This could use some refactoring
+   *
+   * @param {Object} opts The options passed into the constructor
+   */
+  Posepointer.setDefaults = function (opts) {
+    this.initOptions = opts
+
+    // Setup defaults
+    this.options = {
+      autostart: typeof opts.autostart !== 'undefined' ? opts.autostart : true,
+      facingMode: opts.facingMode || 'user',
+      video: opts.video || util.createDefaultVideo(),
+      canvas: opts.canvas || util.createDefaultCanvas(),
+      posenet: {
+        multiplier: opts.posenet && opts.posenet.multiplier ? opts.posenet.multiplier : 0.75,
+        maxUsers: opts.posenet && opts.posenet.maxUsers ? opts.posenet.maxUsers : 1,
+        minPoseConfidence: opts.posenet && opts.posenet.minPoseConfidence ? opts.posenet.minPoseConfidence : 0.1,
+        minPartConfidence: opts.posenet && opts.posenet.minPartConfidence ? opts.posenet.minPartConfidence : 0.5,
+        nmsRadius: opts.posenet && opts.posenet.nmsRadius ? opts.posenet.nmsRadius : 20
+      }
+    }
+  }
+
+  /**
+   * # PRIVATE METHOD
+   *
+   * Applies aliases to common options
+   */
+  Posepointer.setAliases = function () {
+    this.video = this.options.video
+    this.canvas = this.options.canvas
+  }
+
+  /**
+   * # PRIVATE METHOD
+   *
+   * Sets up the webcam and stream: @TODO
+   * [] This will create its own video/canvas elements, allowing you to have
+   *    multiple instances going (for example, to use front/back cameras
+   *    simultaneously)
+   * [] Recreates the video feed to reassign srcObject
+   */
+  Posepointer.setupFeed = async function () {
+    // Set webcam dimensions
+    this.canvas.width = this.video.width = 600
+    this.canvas.height = this.video.height = 500
+
+    // Start the stream based on the device
+    const isMobile = util.isMobile()
+    this.video.srcObject = await navigator.mediaDevices.getUserMedia({
+      // We only care about the camera
+      audio: false,
+      video: {facingMode: this.options.facingMode},
+      width: isMobile ? undefined : this.video.width,
+      height: isMobile ? undefined : this.video.height
+    })
+    this.video.play()
+  }
+
+  /**
+   * # PRIVATE METHOD
+   *
+   * Initializes PoseNet and starts the tracking loop: @TODO
+   * [] This loads a model from Google's servers based on the chosen PoseNet
+   *    modifier
+   */
+  Posepointer.initPoseNet = async function () {
+    this.posenet = await posenet.load(this.options.posenet.multiplier)
+    this._isTracking = true
+    this.constructor.trackPosesLoop(this)
+  }
+
+  /**
+   * # PRIVATE METHOD
+   *
+   * Recursive method for tracking poses on each animationFrame: @TODO
+   * [] This method is recursive, once called it continues until after
+   *    posepointer.stop() is called or until this._isTracking is false
+   *
+   * @param {PosePointer} context The this context, since we're in the
+   *    constructor scope now
+   */
+  Posepointer.trackPosesLoop = async function (context) {
+    context.trackPoses()
+    context._isTracking && requestAnimationFrame(() => this.trackPosesLoop(context))
+  }
+}
