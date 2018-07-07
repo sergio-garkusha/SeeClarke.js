adbfd1f1e95379744a449157596b0f4fae25a354
Oz Ramos <labofoz@gmail.com>
2018-07-06 18:39:07 -0700

    #2 Adds static methods, defaults for posenet configs, creates canvas

--- src/index.js
+++ src/index.js
@@ -24,12 +24,12 @@ module.exports = class PosePointer {
    * @param {OBJ} opts Our initializer options, @see /wiki/Options.md
    */
   constructor (opts = {}) {
-    this.setDefaults(opts)
-    this.mapDefaults()
+    this.constructor.setDefaults.call(this, opts)
+    this.constructor.mapDefaults.call(this)
 
     if (this.options.autostart) {
-      this.setupFeed()
-      this.initPoseNet()
+      this.constructor.setupFeed.call(this)
+      this.constructor.initPoseNet.call(this)
     }
   }
 
@@ -37,16 +37,21 @@ module.exports = class PosePointer {
    * Sets the default options, and overwrites this.originalOpts
    * @param {Object} opts Our initializer options, @see /wiki/Options.md
    */
-  setDefaults (opts) {
+  static setDefaults (opts) {
     this.initOptions = opts
 
     // Setup defaults
     this.options = {
       autostart: typeof opts.autostart !== 'undefined' ? opts.autostart : true,
       facingMode: opts.facingMode || 'user',
-      feed: opts.feed || util.createDefaultVideoFeed(),
+      video: opts.video || util.createDefaultVideo(),
+      canvas: opts.canvas || util.createDefaultCanvas(),
       posenet: {
-        multiplier: opts.posenet && opts.posenet.multiplier ? opts.posenet.multiplier : 0.75
+        multiplier: opts.posenet && opts.posenet.multiplier ? opts.posenet.multiplier : 0.75,
+        maxUsers: opts.posenet && opts.posenet.maxUsers ? opts.posenet.maxUsers : 1,
+        minPoseConfidence: opts.posenet && opts.posenet.minPoseConfidence ? opts.posenet.minPoseConfidence : 0.1,
+        minPartConfidence: opts.posenet && opts.posenet.minPartConfidence ? opts.posenet.minPartConfidence : 0.5,
+        nmsRadius: opts.posenet && opts.posenet.nmsRadius ? opts.posenet.nmsRadius : 20
       }
     }
   }
@@ -54,37 +59,50 @@ module.exports = class PosePointer {
   /**
    * Maps properties to the defaults (or in other words sets property aliases)
    */
-  mapDefaults () {
-    this.feed = this.options.feed
+  static mapDefaults () {
+    this.video = this.options.video
+    this.canvas = this.options.canvas
   }
 
   /**
    * Sets up the webcam and stream
    */
-  async setupFeed () {
+  static async setupFeed () {
     // Error out when webcams are not supported
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia || !util.isWebGLSupported()) throw new Error('ERROR: This browser does not support webcams, please try another browser...like Google Chrome!')
 
     // Set webcam dimensions
-    this.feed.width = 600
-    this.feed.height = 500
+    this.canvas.width = this.video.width = 600
+    this.canvas.height = this.video.height = 500
 
     // Start the stream based on the device
     const isMobile = util.isMobile()
-    this.feed.srcObject = await navigator.mediaDevices.getUserMedia({
+    this.video.srcObject = await navigator.mediaDevices.getUserMedia({
       // We only care about the camera
       audio: false,
       video: {facingMode: this.options.facingMode},
-      width: isMobile ? undefined : this.feed.width,
-      height: isMobile ? undefined : this.feed.height
+      width: isMobile ? undefined : this.video.width,
+      height: isMobile ? undefined : this.video.height
     })
-    this.feed.play()
+    this.video.play()
   }
 
   /**
-   * Initializes posenet
+   * Initializes PoseNet and starts the tracker
    */
-  async initPoseNet () {
+  static async initPoseNet () {
     this.posenet = await posenet.load(this.options.posenet.multiplier)
+    this.trackPoses()
+  }
+
+  /**
+   * !!! Tracks poses !!!
+   *
+   * This method is recursive, once called it continues until posepointer.stop() is called
+   */
+  async trackPoses () {
+    let poses = []
+    let minPoseConfidence = this.options.posenet.minPoseConfidence
+    let minPartConfidence = this.options.posenet.minPartConfidence
   }
 }
--- src/util.js
+++ src/util.js
@@ -8,13 +8,26 @@ module.exports = {
    *
    * @return {HTMLVideoElement} A hidden video used for inference with PoseNet
    */
-  createDefaultVideoFeed () {
+  createDefaultVideo () {
     const $video = document.createElement('video')
     $video.setAttribute('playsinline', true)
     $video.style.transform = 'scale(-1, 1)'
     document.body.appendChild($video)
 
     return $video
+  },
+
+  /**
+   * Creates a default (flipped) canvas and adds it to the DOM
+   *
+   * @return {HTMLCanvasElement} A hidden canvas used for debugging with PoseNet
+   */
+  createDefaultCanvas () {
+    const $canvas = document.createElement('canvas')
+    $canvas.style.transform = 'scale(-1, 1)'
+    document.body.appendChild($canvas)
+
+    return $canvas
   },
 
   /**
